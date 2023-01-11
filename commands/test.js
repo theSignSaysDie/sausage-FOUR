cb4bff146d7a22535defec31cc0a59adbbc497b8
Daniel Angres <gdnlclub@gmail.com>
2023-01-11 14:03:48 -0800

    Slash command infrastructure set up

---
+++ commands/test.js
@@ -0,0 +1,7 @@
+const { SlashCommandBuilder } = require('discord.js');
+module.exports = {
+	data: new SlashCommandBuilder().setName('test').setDescription('Simple Test Command!'),
+	async execute(interaction) {
+		await interaction.reply('Hai!');
+	},
+};
