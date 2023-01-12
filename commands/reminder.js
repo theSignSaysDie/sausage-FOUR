197c28831d6f1c9ead20815e3ea84637d7414a16
Daniel Angres <gdnlclub@gmail.com>
2023-01-12 01:02:59 -0800

    Implemented /reminder command

---
+++ commands/reminder.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { colorDict } = require('../utils/info');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('reminder')
+		.setDescription('Remember the important things'),
+	async execute(interaction) {
+		const embed = new EmbedBuilder()
+			.setColor(colorDict.SAUSAGE_UPDATE)
+			.setDescription(`
+			Hi! This is a reminder for you to do that thing you haven't done yet!
+			Stretch if you need to, take your meds, drink some water, have something to eat. Take care of yourself!
+		`);
+		await interaction.reply({ embeds: [embed] });
+	},
+};
