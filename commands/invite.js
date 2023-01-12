8e86d0e653683c56941e1db91d5fa9cd88e855ef
Daniel Angres <gdnlclub@gmail.com>
2023-01-12 15:46:27 -0800

    Finished /invite command

--- commands/invite.js
+++ commands/invite.js
@@ -1,9 +1,16 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('invite')
 		.setDescription('Generate an invite link'),
 	async execute(interaction) {
-		await interaction.reply({ content: 'https://discord.com/api/oauth2/authorize?client_id=1057044181513683095&permissions=414464666688&scope=bot%20applications.commands', ephemeral: true });
+		const row = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setLabel('Invite Sausage!')
+					.setStyle(ButtonStyle.Link)
+					.setURL('https://discord.com/api/oauth2/authorize?client_id=1057044181513683095&permissions=414464666688&scope=bot%20applications.commands'),
+			);
+		await interaction.reply({ components: [row] });
 	},
 };
