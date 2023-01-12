eeaabdf4dd6ca54dfdfa5208a28c76aaa61709c3
Daniel Angres <gdnlclub@gmail.com>
2023-01-11 19:29:08 -0800

    Cleaned up whitespace in roll

--- commands/roll.js
+++ commands/roll.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { formatRoll, modStr } = require('../utils/dice');
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,23 +8,11 @@ module.exports = {
 			option.setName('talent')
 				.setDescription('Specify Talent (default normal)')
 				.addChoices(
-					{
-						name: '😐 normal',
-						value: 0,
-					},
+					{ name: '😐 normal',	value: 0 },
 					{ name: '🙂 talented', value: 1 },
-					{
-						name: '😃 legendary',
-						value: 2,
-					},
-					{
-						name: '🙁 inept',
-						value: -1,
-					},
-					{
-						name: '😧 godawful',
-						value: -2,
-					},
+					{ name: '😃 legendary',	value: 2 },
+					{ name: '🙁 inept',	value: -1 },
+					{ name: '😧 godawful', value: -2 },
 				),
 		).addIntegerOption(option =>
 			option.setName('modifier')
@@ -35,22 +23,11 @@ module.exports = {
 		).addIntegerOption(option =>
 			option.setName('dice')
 				.setDescription('🎲 FOR [METAGAMERS] ONLY 🎲')
-				.addChoices({
-					name: '2d8',
-					value: 8,
-				},
-				{
-					name: '4d4',
-					value: 4,
-				},
-				{
-					name: '1d6+1d10',
-					value: 10,
-				},
-				{
-					name: '8d2',
-					value: 2,
-				}),
+				.addChoices(
+					{ name: '2d8', value: 8 },
+					{ name: '4d4', value: 4 },
+					{ name: '1d6+1d10',	value: 10 },
+					{ name: '8d2', value: 2 }),
 		),
 	async execute(interaction) {
 		const dice = interaction.options.getInteger('dice') || 8;
@@ -61,6 +38,10 @@ module.exports = {
 		const talentName = ['godawful', 'inept', '', 'talented', 'legendary'];
 		const metagamerModifier = (dice == 8) ? '' : ` [\`${{ 10: '1d6+1d10', 4: '4d4', 2: '8d2' }[dice]}\`]`;
 		const diceString = (talent == 0 && modifier == 0 && dice == 8) ? '' : ` (${dice == 2 ? '' : talentName[talent + 2]}${dice == 2 ? '' : ' ' + modStr(modifier)}${metagamerModifier})`;
-		await interaction.reply(`**__${description}__**${diceString}:\n🎲 ${formatRoll(dice, talent, modifier)}`);
+		const embed = new EmbedBuilder()
+			.setTitle(`**__${description}__**${diceString}`)
+			.setDescription(`🎲 ${formatRoll(dice, talent, modifier)}`);
+		await interaction.reply({ embeds: [embed] });
+		// await interaction.reply(`**__${description}__**${diceString}:\n🎲 ${formatRoll(dice, talent, modifier)}`);
 	},
 };
