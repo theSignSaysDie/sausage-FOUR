e97828844eb8a93d4716cb7bb4d4296713bd1dc1
Daniel Angres <gdnlclub@gmail.com>
2023-01-12 14:22:52 -0800

    Revised reminders (copy missing)

--- commands/version.js
+++ commands/version.js
@@ -10,26 +10,15 @@ module.exports = {
 			.setTitle(`SAUSAGE ${versionNum}`)
 			.setColor(colorDict.SAUSAGE_UPDATE)
 			.setDescription(`
-				**LAST UPDATE:** December 15th, 2021 @ 19:45 EST\n
-                **AUTHOR**: Neo\n\n
-                <:robofrog:853005327694561310> **GENERAL UPDATES**\n\n
-                The troll whitelist is now added to the fanmade docs.\n\n
-            	<:robofrog:853005327694561310> **MECHANIC UPDATES**\n
-                The following things have been updated to match the versions on the docs.\n\n
-                Psimates (Mutants)\n
-                Gobblefiend (Monsters)\n
-                Mutagenic Adaptation (Violet)\n
-                Neighborhood Watch Doc & Moves Added\n
-                Orthodox Decompositionist Doc & Moves Added\n
-                Scalemates (Precaptchalogues)\n
-                Recharge (Tag)\n
-                Peaceful (Emotion)\n
-                Serene (Emotion)\n\n
-				<:robofrog:853005327694561310> **REMINDERS**\n\n
-                Doing ?tipjar brings up the ko-fi's and paypal's of the creators! We appreciate any tips, as we make no money off the upkeep of Sausage. \n
-                You may notice some moves are formatted differently from others. We are in the process of changing the formatting of moves within Sausage!
-                Moves will be changed over to the new layout as they are patched, or by bulk when one of the devs has the time.
-			`);
+				**LAST UPDATE:** December 15th, 2021 @ 19:45 EST
+
+                **AUTHORS**: Neo, Meme
+
+                <:robofrog:853005327694561310> **GENERAL UPDATES**
+                TODO
+            	
+                <:robofrog:853005327694561310> **MECHANIC UPDATES**
+                TODO`);
 		await interaction.reply({ embeds: [embed] });
 	},
 };
