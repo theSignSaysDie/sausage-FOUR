4442bc60d4ac0202e41d30081859f50e7595ef50
Daniel Angres <gdnlclub@gmail.com>
2023-02-04 19:11:03 -0800

    Implemented /choose command

---
+++ commands/choose.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { roll1ToX } = require('../utils/dice');
+
+const delimiters = ';/, ';
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('choose')
+		.setDescription('Choose from a list of choices')
+		.addStringOption(
+			option =>
+				option
+					.setName('choices')
+					.setDescription('What you\'re picking from')
+					.setRequired(true),
+		),
+	async execute(interaction) {
+		const choiceString = interaction.options.getString('choices');
+		let choices;
+		if (choiceString.indexOf(' ') === -1) {
+			await interaction.reply({ content: 'dude.', ephemeral: true });
+			return;
+		}
+		for (const s in delimiters) {
+			if (choiceString.indexOf(delimiters[s]) !== -1) {
+				choices = choiceString.split(delimiters[s]);
+				break;
+			}
+		}
+		await interaction.reply({ content: `Choice selected: \`${choices[roll1ToX(choices.length) - 1].trim()}\``, ephemeral: true });
+	},
+};
