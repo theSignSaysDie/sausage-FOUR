7be1696e008c34bd7c774646c9bfdf006566abee
Daniel Angres <gdnlclub@gmail.com>
2023-02-21 08:57:34 -0800

    version command has info now

--- commands/version.js
+++ commands/version.js
@@ -12,13 +12,19 @@ module.exports = {
 			.setDescription(`
 				**LAST UPDATE:** ${lastUpdated.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', timeZoneName: 'short' }).replace('at', '@')}
 
-                **AUTHORS**: Neo, Meme
+                **AUTHORS**: <@308760900016537610>, <@315220045141770241>
 
                 <:robofrog:853005327694561310> **GENERAL UPDATES**
-                TODO
+                - Migrated pre-existing functionality to \`/\` slash commands for more power and flexibility (and to avoid getting bonked by Discord API obsolescence)
+				- Added \`/lookup\` - replaces \`move\`, \`status\`, and similar commands
+				- Improved \`/trollcall\` - Troll Calls can now be registered per user and fetched by anyone
+				- Added \`/starter\` - Trollcial post templates (intro, post, and outro) can be registered and retrieved by troll for easier trolling on mobile
+				- Added \`/pinglist\` - Pinglists can now be registered by user and joined/left without needing to bother the owner
+				- Added \`/roll\` - Sausage can roll dice now, and uses an even better random generator than Avrae does
+				- Added \`/choose\` - just a bit better than Clamkiller's
             	
                 <:robofrog:853005327694561310> **MECHANIC UPDATES**
-                TODO`);
-		await interaction.reply({ embeds: [embed] });
+				- None! Game info is no longer hard-coded and can be patched without rebooting Sausage!`);
+		await interaction.reply({ embeds: [embed], allowedMentions: { users: [] } });
 	},
 };
