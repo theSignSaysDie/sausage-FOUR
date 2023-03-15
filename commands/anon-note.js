480f9af2031b20de223f37c522a39bb65099ad47
Daniel Angres <gdnlclub@gmail.com>
2023-03-14 19:35:06 -1000

    Diagnostic patch, don't mind it

--- commands/anon-note.js
+++ commands/anon-note.js
@@ -5,9 +5,13 @@ module.exports = {
 		.setDescription('Report an issue anonymously'),
 	async execute(interaction) {
 		try {
+			console.log(interaction.user.nickname);
+			console.log('Fetching guild...');
 			const guild = await interaction.client.guilds.cache.get(process.env.GUILD_ID);
 			// Throws error if user isn't in the guild
+			console.log(interaction.user.id);
 			await guild.members.fetch(interaction.user.id);
+			console.log('Building action row and text input...');
 			const noteRow = new ActionRowBuilder();
 			const noteBox = new TextInputBuilder()
 				.setCustomId('anonNote_content')
@@ -21,9 +25,11 @@ module.exports = {
 				.setCustomId('anonNoteModal')
 				.setTitle('Submit Anonymous Note')
 				.addComponents(noteRow);
-
+			console.log('Showing modal....');
 			await interaction.showModal(modal);
-		} catch {
+		} catch (e) {
+			console.log('Uh oh spaghettios');
+			console.log(e);
 			await interaction.reply('Sorry, but you don\'t have permission to use this command.');
 		}
 	},
