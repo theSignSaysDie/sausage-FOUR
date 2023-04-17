60ceed1ddf797c2129bb3357f187133c448c372d
Daniel Angres <gdnlclub@gmail.com>
2023-04-17 09:58:44 -0700

    when the misery is unending

--- utils/info.js
+++ utils/info.js
@@ -266,8 +266,8 @@ Just this! No other arguments. A modal will pop up where you can type your messa
 
 module.exports = {
 	CHAR_LIMIT: 4000,
-	versionNum: 'v3.5.2',
-	lastUpdated: new Date('April 16, 2023 14:48 PST'),
+	versionNum: 'v3.5.3',
+	lastUpdated: new Date('April 17, 2023 09:58 PST'),
 	colorDict: colorDict,
 	docDict: docDict,
 	fanmadeLinks: fanmadeLinks,
