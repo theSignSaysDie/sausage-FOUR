5609a426ff29412941205c18c2140264981b0d74
Daniel Angres <gdnlclub@gmail.com>
2023-07-03 14:44:11 -0700

    /cardtest now calls specific cards

--- commands/cardtest.js
+++ commands/cardtest.js
@@ -1,13 +1,22 @@
 /* eslint-disable capitalized-comments */
 const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const { generateCard } = require('../utils/cards');
+const { getDefaultEmbed } = require('../utils/stringy');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('cardtest')
-		.setDescription('Simple card test command'),
+		.setDescription('Simple card test command')
+		.addStringOption(option =>
+			option
+				.setName('card')
+				.setDescription('Which card do you want to see?')
+				.setRequired(true),
+		),
 	async execute(interaction) {
-		const card = await generateCard('kaiju_2023', 'banansa');
-		const attachment = new AttachmentBuilder(card, { name: 'test.png' });
-		await interaction.reply({ files: [attachment] });
+		const card = await generateCard('kaiju_2023', interaction.options.getString('card'));
+		const attachment = new AttachmentBuilder(card, { name: 'card.png' });
+		const embed = getDefaultEmbed()
+			.setImage('attachment://card.png');
+		await interaction.reply({ embeds: [embed], files: [attachment] });
 	},
 };
