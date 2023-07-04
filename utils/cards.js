0d2261ec9cee19055a23e74d9a3892eb3528371e
Daniel Angres <gdnlclub@gmail.com>
2023-07-04 12:42:50 -0700

    Removed horrorterrors

--- utils/cards.js
+++ utils/cards.js
@@ -8,10 +8,13 @@ const GradientAlignment = {
 };
 
 /**
+ * Creates a gradient from a list of stops and colors, either vertically or horizontally
  * @param {Canvas.SKRSContext2D} ctx The context to which the gradient belongs
  * @param {Canvas.Canvas} canvas The canvas upon which the gradient is drawn
  * @param {GradientAlignment} align The alignment of the gradient (assumed to be the size of the image)
- * @param {Array} stops A dictionary of stops on interval [0, 1] and their corresponding colors
+ * @param {Array} stop_points A list of stops on interval [0, 1]
+ * @param {Array} stop_colors A list of the colors for each stop
+ * @returns {CanvasGradient} A finished gradient for use as a path fill
 */
 function makeGradient(ctx, canvas, align, stop_points, stop_colors) {
 	const grd = ctx.createLinearGradient(0, 0, canvas.width * (align === GradientAlignment.HORIZONTAL), canvas.height * (align === GradientAlignment.VERTICAL));
