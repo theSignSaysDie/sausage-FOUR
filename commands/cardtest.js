65d781e9b9ff9d63a7f92b4c5d0ae2288dafbda3
Daniel Angres <gdnlclub@gmail.com>
2023-08-05 12:07:17 -0700

    Added subcommand structure for cardtest

--- commands/cardtest.js
+++ commands/cardtest.js
@@ -6,17 +6,36 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('cardtest')
 		.setDescription('Simple card test command')
-		.addStringOption(option =>
-			option
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('binder')
+				.setDescription('View the cards you\'ve collected'),
+		).addSubcommand(subcommand =>
+			subcommand
 				.setName('card')
-				.setDescription('Which card do you want to see?')
-				.setRequired(true),
+				.setDescription('View a particular card')
+				.addStringOption(option =>
+					option
+						.setName('name')
+						.setDescription('Which card do you want to see?')
+						.setRequired(true),
+				),
+		).addSubcommand(subcommand =>
+			subcommand
+				.setName('trade')
+				.setDescription('Trade cards with another user'),
 		),
 	async execute(interaction) {
-		const card = await generateCard('kaiju_2023', interaction.options.getString('card'));
-		const attachment = new AttachmentBuilder(card, { name: 'card.png' });
-		const embed = getDefaultEmbed()
-			.setImage('attachment://card.png');
-		await interaction.reply({ embeds: [embed], files: [attachment] });
+		if (interaction.options.getSubcommand() === 'binder') {
+			return;
+		} else if (interaction.options.getSubcommand() === 'card') {
+			const card = await generateCard('kaiju_2023', interaction.options.getString('name'));
+			const attachment = new AttachmentBuilder(card, { name: 'card.png' });
+			const embed = getDefaultEmbed()
+				.setImage('attachment://card.png');
+			await interaction.reply({ embeds: [embed], files: [attachment], ephemeral: true });
+		} else if (interaction.options.getSubcommand() === 'trade') {
+			return;
+		}
 	},
 };
