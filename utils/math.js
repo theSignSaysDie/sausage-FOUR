f1efb0f9abb2f1360cb00feda1b4aa21bf993ade
Daniel Angres <gdnlclub@gmail.com>
2023-08-05 13:17:55 -0700

    Added util methods for easy mapping

--- utils/math.js
+++ utils/math.js
@@ -6,7 +6,23 @@ function zip(a, b) {
 	return a.map((e, i) => [e, b[i]]);
 }
 
+function objectMap(object, mapFn) {
+	return Object.keys(object).reduce(function(result, key) {
+		result[key] = mapFn(object[key]);
+		return result;
+	}, {});
+}
+
+function objectToListMap(object, mapFn) {
+	return Object.keys(object).reduce(function(result, key) {
+		result.push(mapFn(object[key]));
+		return result;
+	}, []);
+}
+
 module.exports = {
 	round: round,
 	zip: zip,
+	objectMap: objectMap,
+	objectToListMap: objectToListMap,
 };
