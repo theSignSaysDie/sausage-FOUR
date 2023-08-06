7a7bea42f370aa0e2c3ff3bed315bd43ba7472d5
Daniel Angres <gdnlclub@gmail.com>
2023-08-05 19:19:21 -0700

    Added, clamp, parseInt/toString for radix [2,64]

--- utils/math.js
+++ utils/math.js
@@ -1,5 +1,11 @@
+const numerics = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+
 function round(value, decimals) {
 	return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+
+function clamp(x, a, b) {
+	return Math.max(Math.min(x, b), a);
 }
 
 function zip(a, b) {
@@ -20,9 +26,52 @@ function objectToListMap(object, mapFn) {
 	}, []);
 }
 
+function parseInt64(num, radix = 10) {
+	if (radix % 1 !== 0 || (radix < 2 || radix > 64)) {
+		throw new Error(`Radix out of range [2, 64]! (Received ${radix})`);
+	} else {
+		const allowedRange = numerics.slice(0, radix);
+		const regex = `[${allowedRange}]+$`;
+		if (!num.match(regex)) {
+			throw new Error(`Number provided is not valid in base ${radix}! (${num})`);
+		} else {
+			return [...num].reduce((r, a) => r * radix + numerics.slice(0, radix).indexOf(a), 0);
+		}
+	}
+}
+
+function logBase(n, b) {
+	return Math.log(b) / Math.log(n);
+}
+
+function toString64(num, radix = 10) {
+	if (radix % 1 !== 0 || (radix < 2 || radix > 64)) {
+		throw new Error(`Radix out of range [2, 64]! (Received ${radix})`);
+	} else {
+		let result = '';
+		let currentPlace = Math.floor(logBase(radix, num));
+		let quotient;
+		while (currentPlace >= 0) {
+			quotient = Math.floor(num / (radix ** currentPlace));
+			result += numerics[quotient];
+			num -= quotient * (radix ** currentPlace);
+			currentPlace--;
+		}
+		return result;
+	}
+}
+
+function getCurrentTimestamp() {
+	return new Date().getTime();
+}
+
 module.exports = {
 	round: round,
 	zip: zip,
 	objectMap: objectMap,
 	objectToListMap: objectToListMap,
+	parseInt64: parseInt64,
+	toString64: toString64,
+	getCurrentTimestamp: getCurrentTimestamp,
+	clamp: clamp,
 };
