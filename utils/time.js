16e8e29c57e8844993c61d95455b6e835e1205c1
Daniel Angres <gdnlclub@gmail.com>
2023-08-06 13:34:05 -0700

    Added JSDoc to time.js

--- utils/time.js
+++ utils/time.js
@@ -1,5 +1,11 @@
 const { create } = require('discord-timestamps');
 
+/**
+ * @desc wrapper for `discord-timestamps.create`
+ * @param {Integer} offset the time in seconds from now to make a timestamp for (ex. `in 4 hours`)
+ * @returns a timestamp
+ */
+// TODO investigate possibly removing this? Maybe this whole file can go
 async function makeRelativeTimestamp(offset) {
 	return await create(offset / 1000, 'relative');
 }
