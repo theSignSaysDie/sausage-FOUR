71d24afdc2f494093a45b5a56ec954fa8ae9667e
Daniel Angres <gdnlclub@gmail.com>
2023-08-06 13:23:14 -0700

    Added JSDoc to math.js

--- utils/math.js
+++ utils/math.js
@@ -1,7 +1,19 @@
+/**
+ * @desc provides simple number rounding functionality
+ * @param {Number} value number to round
+ * @param {Integer} decimals number of decimal places to round to
+ * @returns a rounded number
+ */
 function round(value, decimals) {
 	return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
 
+/**
+ * @desc zips two arrays of equal length `N` into `N` pairs
+ * @param {Array} a the first array
+ * @param {Array} b the second array
+ * @returns an array of two-element arrays
+ */
 function zip(a, b) {
 	return a.map((e, i) => [e, b[i]]);
 }
