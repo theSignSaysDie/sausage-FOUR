2fbdbe30b9a890fb6f81fbb9a0d39c1d16d0e044
Daniel Angres <gdnlclub@gmail.com>
2023-08-12 22:16:12 -0700

    Moved images in /breakdance

--- commands/breakdance.js
+++ commands/breakdance.js
@@ -1,12 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { rollDice } = require('../utils/dice');
+const { MagicImages } = require('../utils/info');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('breakdance')
 		.setDescription('Do a little dance!'),
 	async execute(interaction) {
 		await interaction.reply(
-			rollDice(100)[0] === 96 ? 'https://cdn.discordapp.com/attachments/973998578215292971/1080029136053084230/ezgif-5-5e4aa51e3e.gif' : '<a:dancedetectivedance:847562900419510283>',
+			rollDice(100)[0] === 96 ? MagicImages.SausageBreakdance : MagicImages.DetectiveDance,
 		);
 	},
 };
