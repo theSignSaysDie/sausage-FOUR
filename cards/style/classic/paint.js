e9714c5844b2307f46e6a84d18549c6f813f6467
Daniel Angres <gdnlclub@gmail.com>
2023-09-09 23:10:38 -0500

    Moved resuable paint function

--- cards/style/classic/paint.js
+++ cards/style/classic/paint.js
@@ -1,18 +1,41 @@
 const Canvas = require('@napi-rs/canvas');
 const path = require('node:path');
-const { makeGradient } = require('../../utils/cards');
-const art_dir = './art';
-async function paintCard(data, name) {
-	const { global_settings, card_info } = data;
+const { makeGradient } = require('../../../utils/cards');
+
+const global_settings = {
+	card_width: 900,
+	card_height: 1200,
+	card_border_offset: 20,
+	card_border_thickness: 16,
+	card_corner_roundness: 80,
+	grd_start: 0.5,
+	art_size_x: 700,
+	art_size_y: 850,
+	art_top: 100,
+	card_art_border_thickness: 20,
+	drop_shadow_blur: 15,
+	vert_info_space: 50,
+	info_height: 100,
+	info_corner_roundness: 25,
+	text_font_size: 50,
+	text_modifiers: 'bold',
+	text_font: 'Courier New',
+};
+async function paintCard(data, set, name) {
+	const { card_info } = data;
 	const { card_width, card_height, card_border_offset, card_border_thickness, card_corner_roundness, grd_start, art_size_x, art_size_y, art_top, card_art_border_thickness, drop_shadow_blur, vert_info_space, info_height, info_corner_roundness, text_font_size, text_modifiers, text_font } = global_settings;
 	const { card_name, art_path, card_border_color, bg_grd_start, bg_grd_end, bg_hex_start, bg_hex_end, art_offset_x, art_offset_y, art_scale, card_art_border_color, drop_shadow_color } = card_info.cards[name];
 	const text_format = `${text_modifiers} ${text_font_size}px ${text_font}`;
 	const nameplate_width = art_size_x - vert_info_space - info_height;
 	const max_text_width = nameplate_width - card_border_thickness * 4;
 
+	const art_dir = path.join(__dirname, '../../', `${set}/art/`);
+
 	// Hex svg mask
-	const hexes = await Canvas.loadImage(path.join(__dirname, './art/hex_final.svg'));
-	const shoggothball_logo = await Canvas.loadImage(path.join(__dirname, '../global/Shoggothball_Icon.png'));
+	console.log('BFBFBFH   ', __dirname);
+	const hexes = await Canvas.loadImage(path.join(__dirname, 'assets/hex_final.svg'));
+	console.log(art_dir);
+	const logo = await Canvas.loadImage(path.join(art_dir, 'logo.png'));
 
 	// Hex background layer
 	const canvas_hex = Canvas.createCanvas(card_width, card_height);
@@ -91,7 +114,7 @@ async function paintCard(data, name) {
 	// Add card art
 	ctx_info.globalCompositeOperation = 'destination-over';
 	console.log(__dirname, art_dir, art_path);
-	const card_art = await Canvas.loadImage(path.join(__dirname, `${art_dir}/${art_path}`));
+	const card_art = await Canvas.loadImage(path.join(art_dir, '/', art_path));
 	ctx_info.drawImage(card_art,
 		(card_art.width - (art_size_x / art_scale)) / 2 + art_offset_x, (card_art.height - (art_size_y / art_scale)) / 2 + art_offset_y, art_size_x / art_scale, art_size_y / art_scale,
 		(card_width - art_size_x) / 2, art_top, art_size_x, art_size_y);
@@ -112,8 +135,8 @@ async function paintCard(data, name) {
 	ctx_info.restore();
 
 	// Format logo
-	ctx_noshadow.drawImage(shoggothball_logo,
-		0, 0, shoggothball_logo.width, shoggothball_logo.height,
+	ctx_noshadow.drawImage(logo,
+		0, 0, logo.width, logo.height,
 		(card_width - art_size_x) / 2 + nameplate_width + vert_info_space - card_art_border_thickness / 2, art_top + art_size_y + vert_info_space - card_art_border_thickness / 2, info_height + card_art_border_thickness, info_height + card_art_border_thickness);
 	ctx_info.drawImage(canvas_noshadow, 0, 0);
 
