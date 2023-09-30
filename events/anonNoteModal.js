368f1429276b69174f3c3486267f59fa1089a7f0
Daniel Angres <gdnlclub@gmail.com>
2023-09-30 12:11:20 -0700

    anon note replies are visible in the anon notes channel

--- events/anonNoteModal.js
+++ events/anonNoteModal.js
@@ -7,6 +7,7 @@ module.exports = {
 		if (!interaction.isModalSubmit()) return;
 		const id = interaction.customId;
 		if (id.startsWith('anonNoteModal')) {
+			await interaction.deferReply();
 			const details = id.replace('anonNoteModal_', '').split('_');
 			const [snowflake] = details;
 			const noteText = interaction.fields.getTextInputValue('anonNote_content');
@@ -19,8 +20,9 @@ module.exports = {
 				);
 			const payload = { content: noteText, components: [row] };
 			await interaction.client.guilds.cache.get(process.env.GUILD_ID).channels.cache.get(process.env.ANON_CHANNEL).send(payload);
-			await interaction.reply({ content:'Your message was successfully sent!', allowedMentions: { users: [] }, ephemeral: true });
+			await interaction.editReply({ content:'Your message was successfully sent!', allowedMentions: { users: [] }, ephemeral: true });
 		} else if (id.startsWith('anonNoteReplyModal')) {
+			await interaction.deferReply();
 			const details = id.replace('anonNoteReplyModal_', '').split('_');
 			const [snowflake] = details;
 			const noteText = interaction.fields.getTextInputValue('anonNoteReply_content');
@@ -28,7 +30,7 @@ module.exports = {
 			await interaction.client.users.fetch(snowflake, false).then((user) => {
 				user.send(payload);
 			});
-			await interaction.reply({ content:'Your reply was successfully sent!', allowedMentions: { users: [] }, ephemeral: true });
+			await interaction.editReply({ content:`Your reply was successfully sent:\n\n>>> ${noteText}`, allowedMentions: { users: [] } });
 		}
 	},
 };
