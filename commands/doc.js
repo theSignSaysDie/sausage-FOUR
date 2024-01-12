d15698045bd54a3bddcb272ed8a485f72a4e7d62
Daniel Angres <gdnlclub@gmail.com>
2024-01-11 23:12:38 -0800

    Added support for looking up plural doc names

--- commands/doc.js
+++ commands/doc.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { docDict } = require('../utils/info');
+const { docDict, docPluralForms } = require('../utils/info');
 const { getDefaultEmbed } = require('../utils/stringy');
 const { getDocLink } = require('../utils/db');
 
@@ -15,10 +15,10 @@ module.exports = {
 	async execute(interaction) {
 		const choice = (interaction.options.getString('document') ?? '').replace(' ', '_').toUpperCase();
 		const embed = getDefaultEmbed();
-		const validChoices = Object.keys(docDict);
+		const validChoices = Object.keys(docDict).concat(Object.keys(docPluralForms));
 		if (choice !== '') {
 			if (validChoices.indexOf(choice) > -1) {
-				const docLink = getDocLink(docDict[choice]);
+				const docLink = getDocLink(docDict[choice] ?? docDict[docPluralForms[choice]]);
 				await interaction.reply({ content: docLink });
 			} else {
 				embed.setDescription(`Sorry, I couldn't find any documents matching \`${choice}\`. Check your spelling and try again.`);
