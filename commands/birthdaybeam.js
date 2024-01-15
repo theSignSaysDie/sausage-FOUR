a3082ef5d325c4e77e66471da2deba4d5473c97b
Daniel Angres <gdnlclub@gmail.com>
2024-01-14 19:21:41 -0800

    Put an 8-hour cooldown on /birthdaybeam

--- commands/birthdaybeam.js
+++ commands/birthdaybeam.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { birthdays, birthdayBeamTime } = require('../utils/info');
+const { fetchSQL } = require('../utils/db');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,12 +12,24 @@ module.exports = {
 				.setDescription('Who\'s it gonna be?'),
 		),
 	async execute(interaction) {
-		const target = interaction.options.getUser('target');
-		birthdays.push(target.id);
-		setTimeout(() => {
-			const index = birthdays.indexOf(target.id);
-			if (index > -1) { birthdays.splice(index, 1); }
-		}, birthdayBeamTime);
-		await interaction.reply({ content: `\`${target.username}#${target.discriminator}\` has been **birthday beamed**! Bring on the 🍰 reacts!`, ephemeral: false });
+		const nowMS = Date.now();
+		const query = await fetchSQL(
+			'SELECT `lastBirthdayBeam` FROM `player` WHERE `snowflake` = ?', [interaction.user.id],
+		);
+		const lastBirthdayBeam = query[0].lastBirthdayBeam;
+		if (nowMS - lastBirthdayBeam < 28_800_000) {
+			await interaction.reply('You\'ve already beamed somebody in the past 8 hours! Try again later.');
+		} else {
+			const target = interaction.options.getUser('target');
+			birthdays.push(target.id);
+			setTimeout(() => {
+				const index = birthdays.indexOf(target.id);
+				if (index > -1) { birthdays.splice(index, 1); }
+			}, birthdayBeamTime);
+			await fetchSQL(
+				'UPDATE `player` SET `lastBirthdayBeam` = ? WHERE `snowflake` = ?', [nowMS, interaction.user.id],
+			);
+			await interaction.reply({ content: `\`${target.username}#${target.discriminator}\` has been **birthday beamed**! Bring on the 🍰 reacts!`, ephemeral: false });
+		}
 	},
 };
