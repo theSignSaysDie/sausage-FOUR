07c451710c6cfef9151b589e7a47acd7cfccbb41
Daniel Angres <gdnlclub@gmail.com>
2024-06-17 10:02:09 -0700

    Cleaned up useless import in stringy.js

--- utils/stringy.js
+++ utils/stringy.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require('discord.js');
-const { versionNum, lastUpdated } = require('../utils/info');
+const { versionNum } = require('../utils/info');
 
 /**
  * @desc Capitalizes a string (ex. `"test" => "Test"`)
